8208b55df5bb22f9bd15e2e19890be67a48565bd
🀄️ <hongzhongx@users.noreply.github.com>
2025-01-08 10:58:39 +0800

    init code

---
+++ vendor/umd.js
@@ -0,0 +1,13 @@
+(function(root, factory) {
+  if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
+    /* global require */
+    factory(exports);
+  } else {
+    root.taiyi = root.taiyi || {};
+    factory((root.taiyi.sjcl = {}));
+  }
+})(this, function(exports) {
+
+  // INSERT
+
+});
